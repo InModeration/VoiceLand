aaadacc3d411423720c61a0c1d54282b36a86247
Asichurter <1090489718@qq.com>
2020-05-02 23:17:16 +0800

    数据测试函数的弃用和删除

---
+++ miniprogram/data/data_comment_detail.js
@@ -0,0 +1,98 @@
+module.exports = {
+
+}
+
+var icon_oreki = "https://766f-voice-land-qcrwm-1301811369.tcb.qcloud.la/assets/image/icon/my_icon_1.JPG?sign=efcc6e3fdd70225fb2902d25af488f9c&t=1587899992"
+var icon_2 = "https://766f-voice-land-qcrwm-1301811369.tcb.qcloud.la/assets/image/icon/my_icon_2.jpeg?sign=28d1818aec8326b25cdf17b505ebb2ca&t=1587978273"
+var icon_3 = "https://766f-voice-land-qcrwm-1301811369.tcb.qcloud.la/assets/image/icon/my_icon_3.jpg?sign=cbf5fa25352de065e53bde7c80e0a66b&t=1587978291"
+var icon_4 = "https://766f-voice-land-qcrwm-1301811369.tcb.qcloud.la/assets/image/icon/my_icon_4.jpeg?sign=a605a5553f9520e7dcbdf35d4d7e0aaa&t=1587978306"
+
+var comment_datail = {
+  commenter: {
+    id: 0,
+    icon_path: icon_2,
+    username: "Rebecca"
+  },
+  content: "难道不明白纸质书更贵啊！！！ 若觉得kindle更贵，我觉得要么阅读量太少，那确实没有买kindle的必要。要么买的都是盗版的纸质书？我不清楚不加以评论。。。 另外，用kindle看小说的..",
+  time: "2小时前",
+  replies: [
+    {
+      reply_id: 0,
+      replier: "Asichurter",
+      replier_id: 0,
+      replier_icon: icon_oreki,
+      repliee: "Rebecca",
+      repliee_id: 1,
+      content: "请所有参加过任何抗疫相关志愿活动及服务的同学加入群：943499561，有关信息将在群内发布，谢谢。",
+      time: "半小时前",
+      like_num: 1,
+    },
+    {
+      reply_id: 1,
+      replier: "Rebecca",
+      replier_id: 0,
+      replier_icon: icon_3,
+      repliee: "Asichurter",
+      repliee_id: 1,
+      content: "哦，真该死，这都第十周了吗",
+      time: "刚刚",
+      like_num: 2,
+    },
+    {
+      reply_id: 2,
+      replier: "MagicalHC",
+      replier_id: 0,
+      replier_icon: icon_4,
+      repliee: "Asichurter",
+      repliee_id: 1,
+      content: "黑咖速速赶到！！",
+      time: "1个小时前",
+      like_num: 2,
+    }
+  ],
+  like_num: 7,
+  reply_num: 3
+}
+
+module.exports.comment_detail = comment_datail
+
+// {
+//   "topic_id": "ApeUUJCw5AKbIurbi9PRZQcV6bdkYEvgksEjuPQ0w0kht3Ll",
+//   "main_user_id": "KnGTfXz9d33f2XXbjep1IuisBz2RlHNuOAoIQgwy5vDUpzKF",
+//   "pictures": [],
+//   "content": "  同学们，教育厅已明确高校开始开学，学生返校起始时间。1、目前，川大正在上报开学计划，近日将公布返校时间；2、大家以我通知的返校时间为准，不要提前返校。学校会大致按照先毕业年级，再参与重大课题研究与临床需要医学生，最后其它年级的顺序，安排同学们返校。持续时间大致在半个月左右，请大家不要着急。3、大家先做好返校的思想准备和调整作息。4、大家如果有具体的困难和特殊情况，请大家及时向我反馈哈",
+//   "time": "2小时前",
+//   "replies": [
+//     {
+//       "reply_id": 0,
+//       "replier": "Asichurter",
+//       "replier_id": 0,
+//       "repliee": "Rebecca",
+//       "repliee_id": 1,
+//       "content": "请所有参加过任何抗疫相关志愿活动及服务的同学加入群：943499561，有关信息将在群内发布，谢谢。",
+//       "time": "半小时前",
+//       "like_num": 1
+//     },
+//     {
+//       "reply_id": 1,
+//       "replier": "Rebecca",
+//       "replier_id": 0,
+//       "repliee": "Asichurter",
+//       "repliee_id": 1,
+//       "content": "哦，真该死，这都第十周了吗",
+//       "time": "刚刚",
+//       "like_num": 2
+//     },
+//     {
+//       "reply_id": 2,
+//       "replier": "MagicalHC",
+//       "replier_id": 0,
+//       "repliee": "Asichurter",
+//       "repliee_id": 1,
+//       "content": "黑咖速速赶到！！",
+//       "time": "1个小时前",
+//       "like_num": 2
+//     }
+//   ],
+//   "like_num": 7
+// }
